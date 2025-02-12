7870fe4328b5006337fdcd9c2337c5ad17c6edb1
Sergh-W <115610381+Sergh-W@users.noreply.github.com>
2025-02-13 01:07:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main() {
 
         while (!textFile.eof())
         {
-            char buffer[100];
+            char buffer[102];
             textFile.read(buffer, sizeof(buffer));
             buffer[textFile.gcount()] = '\0';
             std::cout << buffer;
