547ec49edbd126ac1e7414b9230ca9085f729059
Sergh-W <115610381+Sergh-W@users.noreply.github.com>
2025-02-20 11:49:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 
-
-int main() {
+int main()
+{
     std::ifstream textFile;
-    std::string filePath;
+    textFile.open("..\\Gagarin.txt", std::ios::binary);
 
-    std::cout << "Enter the path to the text file\n"
-                 "you want to open (Example: C:\\...\\text.txt):\n";
-    std::getline(std::cin, filePath);           
-    std::cout << std::endl;
-
-    textFile.open(filePath, std::ios::binary);
-
-    if (!textFile.is_open()) {
+    if (!textFile.is_open())
+    {
         std::cerr << "Error opening file!" << std::endl;
-
     }
     else
     {
@@ -25,34 +17,16 @@ int main() {
 
         while (!textFile.eof())
         {
-            char buffer[102];
+            char buffer[20];
             textFile.read(buffer, sizeof(buffer));
-            buffer[textFile.gcount()] = '\0';
-            std::cout << buffer;
-            std::cout << std::endl;
-            std::cout << std::endl << "********** Characters read: "
-                      << textFile.gcount() << " **********"<< std::endl;
-            std::cout << std::endl;
-            std::cout <<"Do you want to continue? (Enter any character\n"
-                        "to continue, 'q' - quit)" << std::endl;
-            char choice;
-            std::cin >> choice;
-            if (choice == 'q') break;
-
-            /*
-            for (int i = 0; i < textFile.gcount(); ++i)
-            {
-                std::cout << buffer[i];
-            }
-            */
-
-            /*
             std::string str(buffer, textFile.gcount());
             std::cout << str << std::endl;
-            */
         }
     }
 
     textFile.close();
     return 0;
 }
+
+   
+
